6f670b11d924c955f65f8681d99a61e088383124
evan <evan.buschor@gmail.com>
2023-09-14 10:53:16 -0400

    add before, after file, as well as nvimtree plugin

---
+++ lua/config/before.lua
@@ -0,0 +1,21 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.wrap = true
+vim.opt.breakindent = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+
+-- Keybinds
+-- save file (:w)
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>',{desc = 'save file'})
+
+-- quit (:q)
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", {desc = "quit file"})

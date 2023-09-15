107736942bda00cfa32d9a37fb8441c4ef538aef
evan <evan.buschor@gmail.com>
2023-09-15 08:28:44 -0400

    add undo history for long running file

--- lua/config/plugins/undotree.lua
+++ lua/config/plugins/undotree.lua
@@ -1,7 +1,9 @@
-return{
+return {
 	"mbbill/undotree",
 	name = "UndoTree",
 	config = function()
 		vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<Cr>")
+		vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+		vim.opt.undofile = true
 	end
 }

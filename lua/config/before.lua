85e0bdad49d9078956e14ca136eccab007e9e1cf
evan <evan.buschor@gmail.com>
2023-09-15 08:34:38 -0400

    update general settings and keybinds

--- lua/config/before.lua
+++ lua/config/before.lua
@@ -5,7 +5,8 @@ vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 vim.opt.wrap = false
 vim.opt.breakindent = false
 vim.opt.tabstop = 2
@@ -26,3 +27,20 @@ vim.keymap.set("n", "<leader>h", "<cmd>wincmd h<cr>", {desc = "focus on window t
 vim.keymap.set("n", "<leader>j", "<cmd>wincmd j<cr>", {desc = "focus on window to the bottom"})
 vim.keymap.set("n", "<leader>k", "<cmd>wincmd k<cr>", {desc = "focus on window to the top"})
 vim.keymap.set("n", "<leader>l", "<cmd>wincmd l<cr>", {desc = "focus on window to the right"})
+
+-- keybindings (these should probably move to own file)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- number of lines before scrolling:
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+
+-- fast update time:
+vim.opt.updatetime = 50
+
+-- colorcolumn
+vim.opt.colorcolumn = "80"
+
+
